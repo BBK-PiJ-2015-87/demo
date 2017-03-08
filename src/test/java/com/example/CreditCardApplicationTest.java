b63c1c357998cab1505b95db6ea5069e66e7c468
vlad <ivanovs223@gmail.com>
2017-03-08 16:29:08 -0500

    Cleaned imports.

--- src/test/java/com/example/CreditCardApplicationTest.java
+++ src/test/java/com/example/CreditCardApplicationTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
