d54afcd04a136db76c3070b6526f2e12ff0344ea
vlad <ivanovs223@gmail.com>
2017-03-08 15:06:08 -0500

    Parsing string and multiline string to credit card.

--- src/main/java/com/example/model/CreditCards.java
+++ src/main/java/com/example/model/CreditCards.java
@@ -1,5 +1,27 @@
 package com.example.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 public class CreditCards {
 
+    public static CreditCard fromString(String card) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM-yyyy");
+        List<String> list = Stream.of(card.split(",")).map(String::trim).collect(toList());
+
+        return new CreditCard(list.get(0), list.get(1), formatter.parse(list.get(2)));
+    }
+
+    public static List<CreditCard> fromMultilineString(String input) {
+        return Stream.of(input.split("\n")).map(line -> { try {
+            return fromString(line);
+        } catch (ParseException e){
+            throw new RuntimeException(e);
+        }
+        }).collect(toList());
+    }
 }
--- src/test/java/com/example/CreditCardsTest.java
+++ src/test/java/com/example/CreditCardsTest.java
@@ -1,10 +1,56 @@
 package com.example;
 
+import com.example.model.CreditCard;
+import com.example.model.CreditCards;
 import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class CreditCardsTest {
 
     @Test
-    public void shouldParse(){}
+    public void shouldConstructCreditCardFromString() throws ParseException {
+        String input = "HSBC Canada   , 5601-2345-3446-5678 ,  Nov-2017  ";
 
+        CreditCard card = CreditCards.fromString(input);
+
+        assertThat(card.getBankName(), is("HSBC Canada"));
+        assertThat(card.getCardNumber(), is("5601-2345-3446-5678"));
+        assertThat(card.getExpieryDate().getYear(), is(117));
+    }
+
+    @Test(expected = ParseException.class)
+    public void shouldThrowAnExceptionWhenDateIsInWrongFormat() throws ParseException {
+        String input = "HSBC Canada   , 5601-2345-3446-5678 ,  InvalidDate  ";
+
+        CreditCards.fromString(input);
+    }
+
+    @Test
+    public void shouldConstructCreditCardsFromMultilineString() throws ParseException {
+        String input =
+                "HSBC Canada   , 5601-2345-3446-5678 ,  Nov-2017  \n" +
+                "Royal Bank of Canada , 4519-4532-4524-2456,  Oct-2017\n" +
+                "American Express, 3786-7334-8965-345, Dec-2018\n" +
+                "\n" +
+                "\n";
+
+        List<CreditCard> cards = CreditCards.fromMultilineString(input);
+
+        assertThat(cards.size(), is(3));
+        assertThat(cards.get(0).getCardNumber(), is("5601-2345-3446-5678"));
+        assertThat(cards.get(1).getBankName(), is("Royal Bank of Canada"));
+        assertThat(cards.get(2).getExpieryDate().getYear(), is(118));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void shouldThrowAnExceptionWhenInvalidInput() throws ParseException {
+        String invalidInput = "HSBC Canada   , 5601-2345-3446-5678 ,  InvalidDate \n";
+
+        CreditCards.fromMultilineString(invalidInput);
+    }
 }
