ebe0a20e84ce63fdffa3a50feb301643345e6f1f
vlad <ivanovs223@gmail.com>
2017-03-08 14:22:37 -0500

    Base class that represents data model of app.

---
+++ src/main/java/com/example/model/CreditCard.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+import java.util.Date;
+
+public class CreditCard {
+    private String bankName;
+    private String cardNumber;
+    private Date expieryDate;
+
+    public CreditCard(String bankName, String cardNumber, Date expieryDate) {
+        this.bankName = bankName;
+        this.cardNumber = cardNumber;
+        this.expieryDate = expieryDate;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public Date getExpieryDate() {
+        return expieryDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CreditCard that = (CreditCard) o;
+
+        if (!bankName.equals(that.bankName)) return false;
+        if (!cardNumber.equals(that.cardNumber)) return false;
+        return expieryDate.equals(that.expieryDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = bankName.hashCode();
+        result = 31 * result + cardNumber.hashCode();
+        result = 31 * result + expieryDate.hashCode();
+        return result;
+    }
+}
